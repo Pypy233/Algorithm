054e1f9ce2aa344c61f6e9c7a0cf7f22d3affa84
py-task <2529716798@qq.com>
2017-10-07 20:48:15 +0800

    unfinished

---
+++ DataStructureHomework/stage1/Postfix.java
@@ -0,0 +1,17 @@
+package stage1;
+
+/**
+ * Created by py on 2017/10/7.
+ */
+public class Postfix {
+    public String toPostfix(String str){
+        Stack<Character> stack;
+        String resStr;
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(Character.isLetter(ch)){
+
+            }
+        }
+    }
+}
