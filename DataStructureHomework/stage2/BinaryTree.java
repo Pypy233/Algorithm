64fa0166fe23681035d9b75b144781173498f822
py-task <2529716798@qq.com>
2017-10-23 19:05:52 +0800

    What a terrible stage2!

---
+++ DataStructureHomework/stage2/BinaryTree.java
@@ -0,0 +1,69 @@
+package stage1;
+
+/**
+ * Created by py on 2017/10/11.
+ */
+
+public class BinaryTree {
+    class BinaryNode {
+        BinaryNode root;
+        BinaryNode lft, rht;
+        String element;
+
+        public BinaryNode(String element) {
+            this.element = element;
+        }
+
+        public BinaryNode(String element, BinaryNode lft, BinaryNode rht) {
+            this.element = element;
+            this.lft = lft;
+            this.rht = rht;
+        }
+    }
+        public BinaryNode createTree(){
+            BinaryNode root = new BinaryNode("a");
+            BinaryNode node1 = new BinaryNode("b");
+            BinaryNode node2 = new BinaryNode("c");
+            BinaryNode node3 = new BinaryNode("d");
+            BinaryNode node4 = new BinaryNode("e");
+            BinaryNode node5 = new BinaryNode("f");
+            BinaryNode node6 = new BinaryNode("g");
+            root = new BinaryNode(root.element, node1, node2);
+            node1.lft = node3;
+            node1.rht = node4;
+            node2.lft = node5;
+            node2.rht = node6;
+            return root;
+        }
+        public void printInfix(BinaryNode root){
+            if(root != null){
+                printInfix(root.lft);
+                System.out.print(root.element);
+                printInfix(root.rht);
+            }
+        }
+        public void printPostfix(BinaryNode root){
+            if(root != null){
+                printInfix(root.lft);
+                printInfix(root.rht);
+                System.out.print(root.element);
+            }
+        }
+        public void printPrefix(BinaryNode root){
+            if(root != null){
+                System.out.print(root.element);
+                printInfix(root.lft);
+                printInfix(root.rht);
+            }
+        }
+        public static void main(String[] args){
+            BinaryTree tree = new BinaryTree();
+            BinaryNode root = tree.createTree();
+            tree.printInfix(root);
+            System.out.println();
+            tree.printPostfix(root);
+            System.out.println();
+            tree.printPrefix(root);
+        }
+
+}
