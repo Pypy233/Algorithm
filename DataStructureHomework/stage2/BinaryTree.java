94ba5a075eea267c8caa04e8f7d49da86d8b588d
py-task <2529716798@qq.com>
2017-11-10 19:24:50 +0800

    BinarySearchTree

--- DataStructureHomework/stage2/BinaryTree.java
+++ DataStructureHomework/stage2/BinaryTree.java
@@ -1,42 +1,59 @@
 package stage2;
 
+import java.util.Scanner;
+
 /**
  * Created by py on 2017/10/11.
  */
 // Assume that the binary tree is composition of string "abcdefg"
     // What's more, the 'a' the root , then b, c , d, e , f and g.
     //Print infix, postfix and prefix of the tree/=.
+
+    /** Later, change the requirements ???... So I change the BinaryTree to
+     * BinarySearch Tree... By input the integers, all the integers shouldn't be '-1'.
+     *
+    **/
 public class BinaryTree {
     class BinaryNode {
         BinaryNode root;
         BinaryNode lft, rht;
-        String element;
+        int element;
 
-        public BinaryNode(String element) {
+        public BinaryNode(int element) {
             this.element = element;
         }
 
-        public BinaryNode(String element, BinaryNode lft, BinaryNode rht) {
+        public BinaryNode(int element, BinaryNode lft, BinaryNode rht) {
             this.element = element;
             this.lft = lft;
             this.rht = rht;
         }
     }
-        public BinaryNode createTree(){
-            BinaryNode root = new BinaryNode("a");
-            BinaryNode node1 = new BinaryNode("b");
-            BinaryNode node2 = new BinaryNode("c");
-            BinaryNode node3 = new BinaryNode("d");
-            BinaryNode node4 = new BinaryNode("e");
-            BinaryNode node5 = new BinaryNode("f");
-            BinaryNode node6 = new BinaryNode("g");
-            root = new BinaryNode(root.element, node1, node2);
-            node1.lft = node3;
-            node1.rht = node4;
-            node2.lft = node5;
-            node2.rht = node6;
-            return root;
+        public BinaryNode insert(int x, BinaryNode t){
+            if(t == null)
+                return new BinaryNode(x, null, null);
+            if(x > t.element)
+                t.rht = insert(x, t.rht);
+            else if(x < t.element)
+                t.lft = insert(x, t.lft);
+            else;
+            return  t;
         }
+        public BinaryNode createTree(){
+            BinaryNode r = null;
+            Scanner input = new Scanner(System.in);
+            while(true){
+                int a = input.nextInt();
+                if(a == -1)
+                    break;
+                else{
+                    r = insert(a, r);
+                }
+            }
+            return r;
+        }
+
+
         public void printInfix(BinaryNode root){
             if(root != null){
                 printInfix(root.lft);
@@ -46,29 +63,47 @@ public class BinaryTree {
         }
         public void printPostfix(BinaryNode root){
             if(root != null){
-                printInfix(root.lft);
-                printInfix(root.rht);
+                printPostfix(root.lft);
+                printPostfix(root.rht);
                 System.out.print(root.element);
             }
         }
         public void printPrefix(BinaryNode root){
             if(root != null){
                 System.out.print(root.element);
-                printInfix(root.lft);
-                printInfix(root.rht);
+                printPrefix(root.lft);
+                printPrefix(root.rht);
             }
+        }
+        public BinaryNode findMax(BinaryNode r){
+            if(r == null)
+                return null;
+            else if(r.rht == null)
+                return r;
+            return findMax(r.rht);
+        }
+        public BinaryNode findMin(BinaryNode r){
+            if(r == null)
+                return null;
+            else if(r.lft == null)
+                return r;
+            return findMin(r.lft);
         }
         public static void main(String[] args){
             BinaryTree tree = new BinaryTree();
-            BinaryNode root = tree.createTree();
+            BinaryNode r = tree.createTree();
+            System.out.println(r);
             System.out.print("Infix:   ");
-            tree.printInfix(root);
+            tree.printInfix(r);
             System.out.println();
             System.out.print("Postfix: ");
-            tree.printPostfix(root);
+            tree.printPostfix(r);
             System.out.println();
             System.out.print("Prefix:  ");
-            tree.printPrefix(root);
+            tree.printPrefix(r);
+            System.out.println();
+            System.out.println("The max element: " + tree.findMax(r).element);
+            System.out.println("The min element: " + tree.findMin(r).element);
         }
 
 }
