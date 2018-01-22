71859d4024f76e717e6ea5f527f96323613ebe94
py-task <2529716798@qq.com>
2018-01-22 18:48:06 +0800

    base_converter

---
+++ py/basic_structure/base_converter.py
@@ -0,0 +1,21 @@
+from basic_structure import Stack
+
+
+def base_converter(dec_number, base):
+    digits = '123456789ABCDEF'
+
+    rem_stack = Stack.Stack()
+    while dec_number > 0:
+        rem = dec_number % base
+        rem_stack.push(rem)
+        dec_number //= base
+
+    base_str = ''
+    while not rem_stack.is_empty():
+        base_str += digits[rem_stack.pop()]
+
+    return base_str
+
+
+print(base_converter(25, 2))
+print(base_converter(25, 16))
