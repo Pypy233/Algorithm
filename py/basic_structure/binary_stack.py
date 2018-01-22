6734eaba4f6a742d3e149e0269e131afd139944c
py-task <2529716798@qq.com>
2018-01-22 17:49:55 +0800

    decimal

---
+++ py/basic_structure/binary_stack.py
@@ -0,0 +1,18 @@
+# Transform decimal number to binary number by using stack defined before.
+# The complexity is O(logn)
+
+from basic_structure import Stack
+def divide_by_2(dec_number):
+    rem_stack = Stack.Stack()
+
+    while dec_number > 0:
+        rem_number = dec_number % 2
+        rem_stack.push(rem_number)
+        dec_number //= 2
+
+    bin_num = ''
+    while not rem_stack.is_empty():
+        bin_num += (str)(rem_stack.pop())
+    return bin_num
+
+print(divide_by_2(42))
