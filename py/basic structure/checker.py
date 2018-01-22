f4cbfb2864cfbfa066aa5daf7cd1674991a7ebcf
py-task <2529716798@qq.com>
2018-01-22 17:08:16 +0800

    finished

--- py/basic structure/checker.py
+++ py/basic structure/checker.py
@@ -8,8 +8,8 @@ def par_checker(symobl_string):
 	balanced = True
 	idx = 0
 	while idx < len(symobl_string) and balanced:
-		symobl = symobl_string[idx]
-		if symobl == '(':
+		symbol = symobl_string[idx]
+		if symbol == '(':
 			s.push(symobl)
 		else:
 			if(s.is_empty()):
@@ -27,3 +27,31 @@ def par_checker(symobl_string):
 
 print(par_checker('(((())))'))
 print(par_checker('((()'))
+
+def par_checker2(symbol_string):
+	s = Stack()
+	balanced = True
+	idx = 0
+	while idx < len(symbol_string) and balanced:
+		symbol = symbol_string[idx]
+		if symbol in '[({' :
+			s.push(symbol)
+		else:
+			if s.is_empty():
+				balanced = False
+			else:
+				top = s.pop()
+				if not matches(top, symbol):
+					return False
+		idx += 1
+		
+	if balanced and s.is_empty():
+		return True
+	else:
+		return False
+
+
+def matches(open, close):
+	opens = '([{'
+	closes = '}])'
+	return opnens.index(open) == closes.index(close)
