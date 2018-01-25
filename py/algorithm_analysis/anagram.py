febf31fce7377595685b5d1a1a065b03a4a1ad98
py-task <2529716798@qq.com>
2018-01-25 15:05:23 +0800

    solution4

--- py/algorithm_analysis/anagram.py
+++ py/algorithm_analysis/anagram.py
@@ -52,5 +52,34 @@ print(anagram_solution2('abcde','edcba'))
 
 
 # The code is simple and readable.
-# The two calls to the Python sort method are not without their own cost, sorting is typically either 𝑂(𝑛2) or 𝑂(𝑛 log 𝑛)
+# The two calls to the Python sort method are not without their own cost, sorting is
+# typically either 𝑂(𝑛2) or 𝑂(𝑛 log 𝑛)
 
+
+def anagram_solution4(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] += 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] += 1
+
+    j = 0
+    still_ok = True
+    while j < 26 and still_ok:
+        if c1[j] == c2[j]:
+            j += 1
+        else:
+            still_ok = False
+
+    return still_ok
+
+
+print(anagram_solution4('apple','pleap'))
+
+# dding it all up gives us 𝑇 (𝑛) = 2𝑛 + 26 steps. That is 𝑂(𝑛).
+# Found a linear order of magnitude algorithm for solving this problem.
