d3a09cbe14535529662efacb6f0c3759d3fa9058
Barney <2529716798@qq.com>
2018-01-25 16:21:07 +0800

    Delete leafNum.cpp

--- D.S.NJU/code & notes/leafNum.cpp
+++
@@ -1,7 +0,0 @@
-int leafNum(BinaryNode<Type> *root){
-	if(root == NULL)
-		return 0;
-	if(root -> lft == NULL && root -> rht == NULL)
-		return 1;
-	return leafNum(root -> lft) + leafNum(root -> rht);
-}
