a068d2bee81b0c6345f1c668712e46d76d79319e
py-task <2529716798@qq.com>
2018-01-25 14:32:54 +0800

    anagram

---
+++ py/algorithm_analysis/anagram.py
@@ -0,0 +1,56 @@
+def anagram_solution(s1, s2):
+    a_list = list(s2)
+
+    pos1 = 0
+    still_ok = True
+
+    while pos1 < len(s1) and still_ok:
+        pos2 = 0
+        found = False
+
+        while pos2 < len(a_list) and not found:
+            if s1[pos1] == a_list[pos2]:
+                found = True
+            else:
+                pos2 += 1
+        if found:
+            a_list[pos2] = None
+        else:
+            still_ok = False
+
+        pos1 += 1
+
+    return still_ok
+
+
+print(anagram_solution('abcd', 'dcba'))
+
+
+# The code is not so simple and readable?
+# The number of visits then becomes the sum of the integers from 1 to 𝑛.
+
+def anagram_solution2(s1, s2):
+    a_list = list(s1)
+    b_list = list(s2)
+
+    a_list.sort()
+    b_list.sort()
+
+    pos = 0
+    matches = True
+
+    while pos < len(s1) and matches:
+        if a_list[pos] == b_list[pos]:
+            pos += 1
+        else:
+            matches = False
+
+    return matches
+
+
+print(anagram_solution2('abcde','edcba'))
+
+
+# The code is simple and readable.
+# The two calls to the Python sort method are not without their own cost, sorting is typically either 𝑂(𝑛2) or 𝑂(𝑛 log 𝑛)
+
