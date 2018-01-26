8405b79ccb693c2e1a8082a4fbc5cea7b93ead6d
py-task <2529716798@qq.com>
2018-01-26 19:32:14 +0800

    spiral

---
+++ py/recursion/draw_spiral.py
@@ -0,0 +1,16 @@
+# Draw a spiral, the effect is inspiring, emmmm...
+import turtle
+
+my_turtle = turtle.Turtle()
+my_win = turtle.Screen()
+
+
+def draw_spiral(my_turtle, line_len):
+    if line_len > 0:
+        my_turtle.forward(line_len)
+        my_turtle.right(90)
+        draw_spiral(my_turtle, line_len - 5)
+
+
+draw_spiral(my_turtle, 100)
+my_win.exitonclick()
