ec30105125be1e7981cca8a1949b5b2afd852432
py-task <2529716798@qq.com>
2018-01-26 22:33:57 +0800

    finished

--- py/sort_and_search/hash_table.py
+++ py/sort_and_search/hash_table.py
@@ -20,7 +20,7 @@ class HashTable:
                 while self.slots[next_slot] != None and \
                         self.slots[next_slot] != key:
                     next_slot = self.rehash(next_slot, len(self.slots))
-                    
+
                 if self.slots[next_slot] == None:
                     self.slots[next_slot] = key
                     self.data[next_slot] = data
@@ -32,3 +32,28 @@ class HashTable:
 
     def rehash(self, old_hash, size):
         return (old_hash + 1) % size
+
+    def get(self, key):
+        start_slot = self.hash_function(key, len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = start_slot
+        while self.slots[position] != None and \
+                not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == start_slot:
+                stop = True
+        return data
+
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
